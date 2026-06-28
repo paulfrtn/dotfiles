970f7f6b5afaa22adb87c8ecac1b2bca0e1a1564
safir <97365678+paulfrtn@users.noreply.github.com>
2026-06-28 23:31:07 +0200

    feat: added power profiles to waybar with some fancy icons

--- nvim/init.lua
+++ nvim/init.lua
@@ -828,7 +828,7 @@ do
       cpp = { 'clang-format' },
       -- rust = { 'rustfmt' },
       -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
+      python = { 'isort', 'black' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
