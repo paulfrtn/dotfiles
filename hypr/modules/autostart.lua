b2fba1ead2a78e8ef302b70483c6d383ed3799f9
safir <97365678+paulfrtn@users.noreply.github.com>
2026-06-28 23:29:08 +0200

    feat: added hypridle with a first config

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -1,11 +1,10 @@
--------------------
+------------------
 ---- AUTOSTART ----
 -------------------
 
-hl.on("hyprland.start", function ()
-    hl.exec_cmd("waybar & awww-daemon")
-    hl.exec_cmd("sleep 1 && waypaper --restore")
-    hl.exec_cmd("sleep 1 && protonvpn-app --start-minimized")
-    hl.exec_cmd("sleep 1 && solaar -w hide")
+hl.on("hyprland.start", function()
+	hl.exec_cmd("waybar & awww-daemon & hypridle")
+	hl.exec_cmd("sleep 1 && waypaper --restore")
+	hl.exec_cmd("sleep 1 && protonvpn-app --start-minimized")
+	hl.exec_cmd("sleep 1 && solaar -w hide")
 end)
-
